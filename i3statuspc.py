b91f559d5a46c26f352116b49ee2800cf108bc85
grzebiel <marcin.grzebieluch@rst.com.pl>
2016-10-03 08:33:48 +0200

    added worktime configuration

--- i3statuspc.py
+++ i3statuspc.py
@@ -4,6 +4,15 @@ from i3pystatus import Status
 from i3pystatus.updates import pacman, cower
 from i3pystatus.weather import weathercom
 
+import socket
+
+host = socket.gethostname()
+isLaptop = False
+
+if host == "PaCC-RST":
+    isLaptop = True
+
+
 COLOR_RED = '#af3a03'
 COLOR_NORMAL = '#fdf4c1'
 
@@ -13,6 +22,21 @@ status = Status(standalone=True)
 status.register("clock",
         format="  %a %-d.%m.%Y   %X",)
 
+
+if isLaptop:
+    status.register(
+            'battery',
+            format=' {consumption} [{status} ]{percentage_design:.2f}% {remaining}',
+            alert=True,
+            alert_percentage=15,
+            status = {
+                'DPL': 'DPL',
+                'CHR': '',
+                'DIS': '',
+                'FULL': '',
+                }
+            )
+
 # Shows the address and up/down state of eth0. If it is up the address is shown in
 # green (the default value of color_up) and the CIDR-address is shown
 # (i.e. 10.10.10.42/24).
@@ -20,8 +44,14 @@ status.register("clock",
 # (defaults of format_down and color_down)
 #
 # Note: the network module requires PyPI package netifaces
+ethernetInterface = "enp0s61f6"
+wifiInterface = "wlp6s0"
+if isLaptop:
+    ethernetInterface = "enp0s25"
+    wifiInterface = "wlp3s0"
+
 status.register("network",
-    interface="enp0s61f6",
+    interface=ethernetInterface,
     format_up=" {v4cidr}",
     format_down="",
     color_up=COLOR_NORMAL,
@@ -29,7 +59,7 @@ status.register("network",
 
 # Note: requires both netifaces and basiciw (for essid and quality)
 status.register("network",
-    interface="wlp6s0",
+    interface=wifiInterface,
     format_up=" {essid} {quality:03.0f}%",
     format_down="",
     color_up=COLOR_NORMAL,)
@@ -73,5 +103,11 @@ status.register("weather",
                     location_code="PLXX0029:1:PL",
                             ),
                     )
+
+if isLaptop:
+    status.register("file",
+        format="{workTime}",
+        components={"workTime": (lambda x: x, "tmp/workTime")}
+        )
 
 status.run()
